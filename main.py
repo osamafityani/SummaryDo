55f48d97c11437d9a5ae6ff03e7499faede1bcc5
osamafityani <osamafitiani2001@gmail.com>
2020-09-14 11:56:10 +0300

    Added flask api to connect with frontend

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
-
-text = """
-"Solids: 100% Cotton; Sport Grey: 90% Cotton, 10% Polyester
-Imported
-Machine wash
-Solids: 100 percent cotton; Sport grey: 90 percent cotton, 10 percent polyester
-Moisture wicking keeps you cool and dry
-Taped neck and shoulders for durability; Tubular rib collar for better stretch and recovery
-Feels soft to the touch; Tag free; Lays flat
-5 pack = SM XL; 4 pack = 2X (Colors may vary)"
-
-"""
+from flask import Flask, request, jsonify, after_this_request
 
 stopWords = list(STOP_WORDS)
-
 nlp = spacy.load('en_core_web_sm')
 
-doc = nlp(text)
 
-tokens =[token for token in doc]
+def textSummarize(text):
+    doc = nlp(text)
 
-wordFrequencies = {}
+    tokens = [token for token in doc]
 
-for word in tokens:
-    wordLower = word.text.lower()
-    if (wordLower not in stopWords) and (wordLower not in punctuation):
-        if wordLower not in wordFrequencies.keys():
-            wordFrequencies[wordLower] = 1
-        else:
-            wordFrequencies[wordLower] += 1
+    wordFrequencies = {}
 
-print(wordFrequencies)
+    for word in tokens:
+        wordLower = word.text.lower()
+        if (wordLower not in stopWords) and (wordLower not in punctuation):
+            if wordLower not in wordFrequencies.keys():
+                wordFrequencies[wordLower] = 1
+            else:
+                wordFrequencies[wordLower] += 1
+    return wordFrequencies
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def upload_data():
+    @after_this_request
+    def add_header(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+    data = request.get_json()
+    print(data['dscr'])
+    return jsonify(textSummarize(data['dscr']))
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8989)
+
+
+
+
+
+
